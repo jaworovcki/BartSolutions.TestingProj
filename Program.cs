316b53e9eb0f3ef1be65820fa7257fdc219385b2
Andrii <jaworovcki@gmail.com>
2024-11-01 19:23:15 +0200

    Register services id di container

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
+using BartSolutionsProject.API.Data;
+using BartSolutionsProject.API.Repositories;
+using BartSolutionsProject.API.Repositories.Interfaces;
+using BartSolutionsProject.API.Services;
+using BartSolutionsProject.API.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+        });
+        
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IAccountsRespository, AccountsRepository>();
+builder.Services.AddScoped<IContactsRepository, ContactsRepository>();
+builder.Services.AddScoped<IIncidentsRepository, IncidentsRepository>();
+builder.Services.AddScoped<IIncidentService, IncidentService>();
 
 var app = builder.Build();
 
@@ -15,5 +36,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
